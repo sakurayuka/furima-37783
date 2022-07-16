73f7e5d336a380c73adf180e7c151c4ad2bec9ee
sakurayuka <104257895+sakurayuka@users.noreply.github.com>
2022-07-16 20:41:36 +0900

    投稿フォームの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,14 @@ class Item < ApplicationRecord
 
 # belongs_to : user
 # has_one : trade
+has_one_attached :image
+
+extend ActiveHash::Associations::ActiveRecordExtensions
+belongs_to :category, :condition, :postage_status, :pref, :day_to_ship
+
+validates :item_name, :price, :explanation, presence: true
+
+validates :category_id, :condition_id, :postage_status_id, :pref_id, :day_to_ship_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+
 end
