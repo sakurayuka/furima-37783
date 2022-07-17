d3ef5214ea1144c18d676c246989d60b03d47cd1
sakurayuka <104257895+sakurayuka@users.noreply.github.com>
2022-07-18 00:46:18 +0900

    商品出品機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,21 @@
 class Item < ApplicationRecord
 
-# belongs_to : user
-# has_one : trade
+  validates :image, presence: true
+  validates :item_name, presence: true
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/ }
+  validates_inclusion_of :price, in:300..9999999
+  validates :explanation, presence: true
+  validates :category_id, :condition_id, :postage_status_id, :pref_id, :day_to_ship_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+#belongs_to : user
+#has_one : trade
 has_one_attached :image
 
 extend ActiveHash::Associations::ActiveRecordExtensions
-belongs_to :category, :condition, :postage_status, :pref, :day_to_ship
-
-validates :item_name, :price, :explanation, presence: true
-
-validates :category_id, :condition_id, :postage_status_id, :pref_id, :day_to_ship_id, numericality: { other_than: 1 , message: "can't be blank"}
-
+belongs_to :category
+belongs_to :condition 
+belongs_to :postage_status 
+belongs_to :pref
+belongs_to :day_to_ship
 
 end
