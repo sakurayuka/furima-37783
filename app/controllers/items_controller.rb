2dee3a8ad92ec833a3d7b262a7a981b94e0a10d2
sakurayuka <104257895+sakurayuka@users.noreply.github.com>
2022-07-23 15:59:32 +0900

    商品情報編集機能の修正_1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :own_item, only: [:edit, :show, :update, :move_to_index]
   before_action :move_to_index, only: [:edit]
+
 
   def index
    @items = Item.all.order("created_at DESC")
@@ -20,11 +22,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -33,11 +33,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
     unless user_signed_in? && current_user.id == @item.user_id
       redirect_to root_path
     end
@@ -46,6 +44,10 @@ class ItemsController < ApplicationController
   private
   def item_params
    params.require(:item).permit(:image, :item_name, :explanation, :price, :category_id, :condition_id, :postage_status_id, :day_to_ship_id, :pref_id).merge(user_id: current_user.id)
+  end
+
+  def own_item
+   @item = Item.find(params[:id])
   end
 
 end
