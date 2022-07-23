c4660f57174467c1442ffeacae738cca712dabc3
sakurayuka <104257895+sakurayuka@users.noreply.github.com>
2022-07-23 16:13:09 +0900

    商品情報編集機能の修正_2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :own_item, only: [:edit, :show, :update, :move_to_index]
+  before_action :own_item, only: [:edit, :show, :update]
   before_action :move_to_index, only: [:edit]
 
 
